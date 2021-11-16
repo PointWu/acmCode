21526cb2fe7ee9d123786f408bf55402c2abdc84
point <wuyuhuiss@outlook.com>
2021-11-16 13:34:02 +0800

    feat:更新几道

---
+++ 2. 两数相加.js
@@ -0,0 +1,34 @@
+/**
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+*/
+/**
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+var addTwoNumbers = function(l1, l2) {
+	let curry = 0,node,prevNode;
+	while (l1 || l2 || curry!=0){
+		const val1 = l1?.val ? l1.val : 0;
+		const val2 = l2?.val ? l2.val : 0;
+		let sum = val1 + val2 + curry;
+		
+		curry = Math.floor(sum / 10);
+		sum = sum % 10;
+		
+		const current = new ListNode(sum);
+		if(prevNode){
+			prevNode.next = current;
+		}else{
+			node = current;
+		}
+		prevNode = current;
+		l1 = l1?.next;
+		l2 = l2?.next;
+	}
+		return node;
+};
